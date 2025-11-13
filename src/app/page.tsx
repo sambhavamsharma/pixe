33c73dfc6095e9dfae18db694801d77946122259
CHEETOS <108931920+cheeetoos@users.noreply.github.com>
2025-11-13 21:41:07 +0530

    03: trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import { Button } from "@/components/ui/button";
+import { getQueryClient, trpc } from "@/trpc/server";
 
-const Page = () => {
-  return <div>
-    <Button>
-      Click Me
-    </Button>
-  </div>;
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { Client } from "./client";
+
+import { Suspense } from "react";
+
+const Page = async () => {  
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "mawu prefetch" }));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+  </HydrationBoundary>
+  );
 }
 
 export default Page;
