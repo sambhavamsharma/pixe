5609fd701425179f7be5652bc6c9e2c957553ad4
CHEETOS <108931920+cheeetoos@users.noreply.github.com>
2025-11-19 05:01:44 +0530

    undo page and db

--- src/app/projects/project/page.tsx
+++ src/app/projects/project/page.tsx
@@ -1,7 +1,42 @@
-"use client";
+// @ts-nocheck
 
-const Page = () => {
-  return <div>Project page loaded on client only.</div>;
+
+import { ProjectView } from "@/modules/projects/ui/views/project-view";
+
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+
+interface Props {
+    params: {
+        projectId: string;
+    };
+}
+
+const Page = async () => {
+    const projectId = ""; // hardcode temporary
+
+
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(
+        trpc.messages.getMany.queryOptions({ projectId })
+    );
+    void queryClient.prefetchQuery(
+        trpc.projects.getOne.queryOptions({ id: projectId })
+    );
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <ErrorBoundary fallback={<p>Error!</p>}>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <ProjectView projectId={projectId} />
+                </Suspense>
+            </ErrorBoundary>
+        </HydrationBoundary>
+    );
 };
 
 export default Page;
